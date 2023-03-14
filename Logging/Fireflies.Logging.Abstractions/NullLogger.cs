6ce336d5fb2bac9fbada5198baf1186bcbe67131
Lars Stenberg <larste@waybler.com>
2023-03-14 17:49:03 +0100

    Fixed logger name for generic types

--- Logging/Fireflies.Logging.Abstractions/NullLogger.cs
+++ Logging/Fireflies.Logging.Abstractions/NullLogger.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Fireflies.Logging.Abstractions;
+﻿namespace Fireflies.Logging.Abstractions;
 
 internal class NullLogger : IFirefliesLogger {
     public void Fatal(Func<string> message) {
--- Logging/Fireflies.Logging.NLog/FirefliesNLogFactory.cs
+++ Logging/Fireflies.Logging.NLog/FirefliesNLogFactory.cs
@@ -5,6 +5,16 @@ namespace Fireflies.Logging.NLog;
 
 public class FirefliesNLogFactory : IFirefliesLoggerFactory {
     public IFirefliesLogger GetLogger<T>() {
-        return new FirefliesNLogLogger(LogManager.GetLogger(typeof(T).Name));
+        var typeName = GetTypeName<T>();
+        return new FirefliesNLogLogger(LogManager.GetLogger(typeName));
+    }
+
+    private static string GetTypeName<T>() {
+        var type = typeof(T);
+        if(!type.IsGenericType)
+            return type.Name;
+
+        var genericArguments = type.GetGenericArguments().Select(x => x.Name).Aggregate((x1, x2) => $"{x1}, {x2}");
+        return $"{type.Name[..type.Name.IndexOf("`", StringComparison.Ordinal)]}<{genericArguments}>";
     }
 }
